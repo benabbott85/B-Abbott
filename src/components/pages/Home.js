ed9cb884303d64acd4306a251a54e5d4f9e2eb92
Ben Abbott <babbott85@gmail.com>
2020-10-21 10:50:04 -0600

    experimented with different positioning

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import NavTabs from "./NavTabs"
-// import {Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 // import {CardImg, Container} from "reactstrap"
 
 function Home() {
@@ -13,21 +13,30 @@ function Home() {
     <br></br>
       <h3 className="denver"> Denver, Colorado</h3>
       <br></br>
+      <br></br>
+      <br></br>
+      <div>
+      <h4 className="tutor">Senior Full Stack Web Development Tutor &</h4>
+    <br></br>
+    <h5 className="ta">Teaching Assistant</h5>
+    <br></br>
+   
      <a href="https://www.linkedin.com/in/ben-abbott-51394421/"><i class="fab fa-linkedin fa-4x linkedin" ></i></a>
       <br></br>
       <a href="https://github.com/benabbott85"><i class="fab fa-github fa-4x github"></i></a>
+      </div>
     <br></br>
 <br></br>    
     <br></br>
 
 
-    <h4 className="tutor">Senior Full Stack Web Development Tutor</h4>
+    {/* <h4 className="tutor">Senior Full Stack Web Development Tutor &</h4>
     <br></br>
     <h5 className="ta">Teaching Assistant</h5>
     <br></br>
-    <p className="tutor">for:</p>
+    <p className="tutor">for:</p> */}
    
-   <a href=""><img className="trillogo" src="https://photos.angel.co/startups/i/2784197-e6b31eeb437202f94e63c4ab4ca1b94f-medium_jpg.jpg?buster=1477923906" alt="trilogo"></img>  </a>   
+   {/* <a href=""><img className="trillogo" src="https://photos.angel.co/startups/i/2784197-e6b31eeb437202f94e63c4ab4ca1b94f-medium_jpg.jpg?buster=1477923906" alt="trilogo"></img>  </a>    */}
 
     
     
--- src/components/pages/Technologies.js
+++ src/components/pages/Technologies.js
@@ -5,140 +5,136 @@ import { Row, Container, Card, Col } from "reactstrap";
 
 function Technologies() {
   return (
-//     <Container>
-//       <Row>
-//   <h1>Below is a list of the different technologies I have used.</h1>
-//   </Row>
-//   <br></br>
-//   <br></br>
-//     <Row>
-//  <Card>
-//    <Col md={3}>
-// <div className="tech1">
+    <Container>
+      <Row>
+  <h1>Below is a list of the different technologies I have used.</h1>
+  </Row>
+  <br></br>
+  <br></br>
+    <Row>
+ <Card>
+   <Col md={3}>
+<div className="tech1">
 
-//             <img className="techimg1" src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/012011/stocklogos-template-01.png?itok=DapYF29D" alt="html"></img>
+            <img className="techimg1" src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/012011/stocklogos-template-01.png?itok=DapYF29D" alt="html"></img>
            
-// </div>
-// </Col>
-// </Card>
-// <Card>
-//   <Col md={3}>
-// <div className="tech2">
+</div>
+</Col>
+</Card>
+
+<Card>
+  <Col md={3}>
+<div className="tech2">
   
-//   <img className="techimg2" src="https://www.w3schools.com/whatis/img_css.jpg" alt="css"></img>
+  <img className="techimg2" src="https://www.w3schools.com/whatis/img_css.jpg" alt="css"></img>
   
-// </div>
-// </Col>
-// </Card>
+</div>
+</Col>
+</Card>
  
-// <Card>
-//   <Col md={3}>
-// <div className="tech3">
+<Card>
+  <Col md={3}>
+<div className="tech3">
   
-//   <img className="techimg3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTtxdAFUa1PtSnn2Rc04H9sAgwVMOvpDgM3koBo9--68F-V5Qd7&usqp=CAU" alt="JS"></img>
+  <img className="techimg3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTtxdAFUa1PtSnn2Rc04H9sAgwVMOvpDgM3koBo9--68F-V5Qd7&usqp=CAU" alt="JS"></img>
   
-// </div>
-// </Col>
-// </Card>
+</div>
+</Col>
+</Card>
 
-// <Card>
-//   <Col md={3}>
-// <div className="tech4">
+<Card>
+  <Col md={3}>
+<div className="tech4">
   
-//   <img className="techimg4" src="https://openjsf.org/wp-content/uploads/sites/84/2019/10/jquery-logo-vertical_large_square.png" alt="Jquery"></img>
+  <img className="techimg4" src="https://openjsf.org/wp-content/uploads/sites/84/2019/10/jquery-logo-vertical_large_square.png" alt="Jquery"></img>
   
-// </div>
-// </Col>
-// </Card>
-//   </Row>
-//   <br></br>
-//   <Row>
-//   <Card>
-//     <Col md ={3}>
-// <div className="tech5">
+</div>
+</Col>
+</Card>
+  </Row>
+  <br></br>
+  <Row>
+  <Card>
+    <Col md ={3}>
+<div className="tech5">
   
-//   <img className="techimg5" src="https://www.techwell.com/sites/default/files/stories/images/cropped_teasers/Beth%20Romanik/2019/node-js-tutorial.png" alt="node"></img>
+  <img className="techimg5" src="https://www.techwell.com/sites/default/files/stories/images/cropped_teasers/Beth%20Romanik/2019/node-js-tutorial.png" alt="node"></img>
   
-// </div>
-// </Col>
-// </Card>
+</div>
+</Col>
+</Card>
 
-// <Card>
-//   <Col md={3}>
-// <div className="tech6">
+<Card>
+  <Col md={3}>
+<div className="tech6">
   
-//   <img className="techimg6" src="https://miro.medium.com/proxy/1*XP-mZOrIqX7OsFInN2ngRQ.png" alt="express"></img>
+  <img className="techimg6" src="https://miro.medium.com/proxy/1*XP-mZOrIqX7OsFInN2ngRQ.png" alt="express"></img>
   
-// </div>
-// </Col>
-// </Card>
+</div>
+</Col>
+</Card>
 
-// <Card>
-//   <Col md={3}>
-// <div className="tech7">
+<Card>
+  <Col md={3}>
+<div className="tech7">
   
-//   <img className="techimg7" src="https://pngimg.com/uploads/mysql/mysql_PNG22.png" alt="mysql"></img>
+  <img className="techimg7" src="https://pngimg.com/uploads/mysql/mysql_PNG22.png" alt="mysql"></img>
   
-// </div>
-// </Col>
-// </Card>
+</div>
+</Col>
+</Card>
 
-// <Card>
-//   <Col md={3}>
-// <div className="tech8">
+<Card>
+  <Col md={3}>
+<div className="tech8">
   
-//   <img className="techimg8" src="https://cdn.iconscout.com/icon/free/png-256/sequelize-1-1175002.png" alt="sequelize"></img>
+  <img className="techimg8" src="https://cdn.iconscout.com/icon/free/png-256/sequelize-1-1175002.png" alt="sequelize"></img>
   
-// </div>
-// </Col>
-// </Card>
-//   </Row>
-//   <br></br>
-//   <Row>
-//     <Card className="empty">
-//       <Col md={2}></Col>
-//     </Card>
+</div>
+</Col>
+</Card>
+  </Row>
+  <br></br>
+  <Row>
+    <Card className="empty">
+      <Col md={2}></Col>
+    </Card>
 
-//     <Card>
-//       <Col md={3}>
-// <div className="tech9">
-// <img className="techimg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQnCo7ZGhvQ6F8Li-sPX2D2c18TvfuuGKNmkQ&usqp=CAU" alt="api"></img>
+    <Card>
+      <Col md={3}>
+<div className="tech9">
+<img className="techimg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQnCo7ZGhvQ6F8Li-sPX2D2c18TvfuuGKNmkQ&usqp=CAU" alt="api"></img>
 
-// </div>
-// </Col>
-//     </Card>
+</div>
+</Col>
+    </Card>
     
-//     <Card>
-//       <Col md={3}>
-// <div className="tech10">
+    <Card>
+      <Col md={3}>
+<div className="tech10">
   
-//   <img className="mongo" src="https://banner2.cleanpng.com/20180702/bgt/kisspng-mongodb-database-nosql-postgresql-mongo-5b39f9e3445fa6.5652746415305261792801.jpg" alt="mongodb"></img>
+  <img className="mongo" src="https://banner2.cleanpng.com/20180702/bgt/kisspng-mongodb-database-nosql-postgresql-mongo-5b39f9e3445fa6.5652746415305261792801.jpg" alt="mongodb"></img>
   
-// </div>
-// </Col>
-// </Card>
-// <Card>
-//   <Col md={3}>
-// <div className="tech11">
+</div>
+</Col>
+</Card>
+<Card>
+  <Col md={3}>
+<div className="tech11">
   
-//   <img className="techimgreact" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="react"></img>
+  <img className="techimgreact" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="react"></img>
   
-// </div>
-// </Col>
-// </Card>
+</div>
+</Col>
+</Card>
 
-// <Card>
+<Card>
 
-// </Card>
-//   </Row>
+</Card>
+  </Row>
   
-//   </Container>
-  <Container>
-<Row>
-  <h1>Below is a list of different technologies I have used</h1>
-</Row>
-
   </Container>
+
   );
 }
 

f1f22a992b99058e0e578835b6b079013785f3de
Ben Abbott <babbott85@gmail.com>
2021-05-12 18:07:01 -0600

    Updates

---
+++ precache-manifest.d7e94f609e4e80b5ba59dcf5937f6c12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee597c6e61d0aa9a9c98d2880c1c7f2a",
+    "url": "/B-Abbott/index.html"
+  },
+  {
+    "revision": "bc57ae79426aad9b3565",
+    "url": "/B-Abbott/static/css/main.ae56e775.chunk.css"
+  },
+  {
+    "revision": "be2ca93eee72c57d0b5f",
+    "url": "/B-Abbott/static/js/2.b63b1c38.chunk.js"
+  },
+  {
+    "revision": "b2df6f42d3b318f0b6ad3605dbfe5698",
+    "url": "/B-Abbott/static/js/2.b63b1c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc57ae79426aad9b3565",
+    "url": "/B-Abbott/static/js/main.f8e28d5a.chunk.js"
+  },
+  {
+    "revision": "6873f9dd18666c97ded5",
+    "url": "/B-Abbott/static/js/runtime-main.44968485.js"
+  },
+  {
+    "revision": "61b76cb519f6bce8872305e6edc632d6",
+    "url": "/B-Abbott/static/media/Ben Abbott_03 (1).61b76cb5.jpg"
+  },
+  {
+    "revision": "f816f7b1df41fe9f4a5cdb46bc84f834",
+    "url": "/B-Abbott/static/media/black background.f816f7b1.jpg"
+  }
+]);
